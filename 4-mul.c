e9dffbd710107426284ae9cb1172a27043f6833c
jojanacido <112574355+jojanacido@users.noreply.github.com>
2022-09-27 22:09:48 -0500

    Add files via upload

---
+++ 4-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+float main ()
+{
+float mul;
+float n1=0;
+float n2=0;
+
+printf("Ingrese un numero:\n");
+scanf("%e",&n1);
+
+printf("Ingrese otro numero:\n");
+scanf("%e",&n2);
+
+mul= (n1 * n2); 
+printf("El resultado de la multiplicacion de los dos numeros es:%f\n",mul);
+return 0;
+}
---
+++ 6-mod.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+int mod;
+int n1=0;
+int n2=0;
+
+printf("Ingrese un numero:\n");
+scanf("%d",&n1);
+
+printf("Ingrese otro numero:\n");
+scanf("%d",&n2);
+
+mod=(n1 % n2);
+ printf("El módulo %d de %d es: %d\n",n1,n2,mod); 
+return 0;
+
+} 
